09dea140265995fa7571b212ec5ec88b3f3d5c57
Eysteinn Örn Jónsson <54945395+eysta00@users.noreply.github.com>
2021-02-24 11:32:17 +0000

    Added SFX to the slime block

--- Assets/Scripts/BouncyPlayer.cs
+++ Assets/Scripts/BouncyPlayer.cs
@@ -6,6 +6,9 @@ namespace AGDDPlatformer
 {
     public class BouncyPlayer : MonoBehaviour
     {
+    [Header("Audio")]
+    public AudioSource source;
+    public AudioClip bounceSound;
     //void OnCollisionEnter2D(Collision2D other)
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -13,6 +16,7 @@ namespace AGDDPlatformer
         if (playerController != null)
             {
                 playerController.velocity.y = (float)Mathf.Sqrt(Mathf.Pow(playerController.velocity.y,2)) * (float)0.95;
+                source.PlayOneShot(bounceSound);
             }
         }
     }
